d591f01e53a63fff9cbfc4cef833fa5a67d8c745
valen cassarino <valencassa@gmail.com>
2022-05-10 12:15:06 -0300

    Added about section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Main from './components/Containers/Main'
+import Main from '../components/Containers/Main'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
