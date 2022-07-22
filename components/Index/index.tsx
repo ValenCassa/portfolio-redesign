7d66e3741b50835224e316f2b3342e7587e5c0e1
valen cassarino <valencassa@gmail.com>
2022-07-22 17:49:07 -0300

    Fixed meta

--- components/Index/index.tsx
+++ components/Index/index.tsx
@@ -1,15 +1,27 @@
 import IndexButtons from "components/Miscellaneous/IndexButtons";
+import Meta from "components/Miscellaneous/Meta";
 import TextIndex from "components/Miscellaneous/TextIndex";
+import Head from "next/head";
 
 const IndexPage = () => {
   return (
-    <div style={{ color: "white" }}>
-      <TextIndex exit>Valentin Cassarino</TextIndex>
-      <TextIndex delay={1.4} exit>
-        Front End Developer
-      </TextIndex>
-      <IndexButtons />
-    </div>
+    <>
+      <Head>
+        <title>Homepage | Valentin Cassarino</title>
+      </Head>
+      <Meta
+        title="Homepage | Valentin Cassarino"
+        description="Go have a look at my portfolio!"
+        image={"https://i.imgur.com/JQOu13Z.png"}
+      />
+      <div style={{ color: "white" }}>
+        <TextIndex exit>Valentin Cassarino</TextIndex>
+        <TextIndex delay={1.4} exit>
+          Front End Developer
+        </TextIndex>
+        <IndexButtons />
+      </div>
+    </>
   );
 };
 
